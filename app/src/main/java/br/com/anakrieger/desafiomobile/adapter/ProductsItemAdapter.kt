c4c0cc838038ba9a44ee751554a1213d1837d91c
Ana Krieger <ana.krieger.dev@gmail.com>
2018-01-15 02:01:34 -0200

    implementando preços e começando a desenhar telas

--- app/src/main/java/br/com/anakrieger/desafiomobile/adapter/ProductsItemAdapter.kt
+++ app/src/main/java/br/com/anakrieger/desafiomobile/adapter/ProductsItemAdapter.kt
@@ -1,6 +1,7 @@
 package br.com.anakrieger.desafiomobile.adapter
 
 import android.content.Context
+import android.graphics.Paint
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -27,8 +28,13 @@ class ProductsItemAdapter(mContext: Context, data: ArrayList<ProductsItem>) : Re
     override fun onBindViewHolder(myViewHolder: ViewHolder, position: Int) {
         val productItem: ProductsItem = listProductsItem[position]
         productItem?.let{ product ->
-            myViewHolder.id?.text = product.id
             myViewHolder.name?.text = product.name
+            product.skus?.get(0)?.sellers?.get(0)?.let{
+                myViewHolder.price?.text = "R$ ${it.price.toString()}"
+                myViewHolder.listPrice?.text = "R$ ${it.listPrice.toString()}"
+                myViewHolder.bestInstallment?.text = "${it.bestInstallment?.count}x de R$ ${it.bestInstallment?.value}"
+            }
+
             myViewHolder.image?.let {
                 loadPicture(it, context = it.context ,url = product.skus?.get(0)?.images?.get(0)?.imageUrl.toString(), transformation = CropSquareTransformation(), drawable = R.drawable.not_available  )
             }
@@ -40,19 +46,26 @@ class ProductsItemAdapter(mContext: Context, data: ArrayList<ProductsItem>) : Re
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-        var id: TextView? = null
         var name: TextView? = null
-        var btnProducts: Button? = null
+//        var btnProducts: Button? = null
         var image: ImageView? = null
+        var price: TextView? = null
+        var listPrice : TextView? = null
+        var bestInstallment : TextView? = null
 
         init {
-            id = itemView.findViewById(R.id.product_id)
             name = itemView.findViewById(R.id.product_name)
             image = itemView.findViewById(R.id.product_image)
+            price = itemView.findViewById(R.id.product_price)
+            bestInstallment = itemView.findViewById(R.id.product_best_installment)
 
-            val btn = itemView.findViewById<Button>(R.id.product_btn)
-            btn.setOnClickListener(this)
-            btnProducts = btn
+            val oldPrice :TextView = itemView.findViewById(R.id.product_list_price)
+            oldPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            listPrice = oldPrice
+
+//            val btn = itemView.findViewById<Button>(R.id.product_btn)
+//            btn.setOnClickListener(this)
+//            btnProducts = btn
         }
 
         override fun onClick(view: View) {
